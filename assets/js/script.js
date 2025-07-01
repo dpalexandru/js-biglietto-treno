17e1fd188d5fa8985059b82567c8ecda4e121356
dpalexandru <alexpoian91@hotmail.com>
2025-07-01 15:28:47 +0200

    [add]variables in js file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Inizializzo le variabili input da utente 
+const km = parseInt(prompt("Quanti chilometri deve percorrere il passeggero?"));
+const eta = parseInt(prompt("Quanti anni ha il passeggero?"));
+
+
+//Costanti calcolo viaggio
+const prezzoAlKm = 0.21;
+const scontoMinorenni = 0.20;
+const scontoPensionati = 0.40;
+
+//calcolo prezzo viaggio 
